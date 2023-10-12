7a3bdb0be5d31456caa8dabb4d42f91e26aab453
Jiwon-Kim <hghg1036@naver.com>
2023-10-12 12:40:25 +0900

    Setting: 개발환경 초기설정

---
+++ src/main/java/com/example/boomerang/BoomerangApplication.java
@@ -0,0 +1,13 @@
+package com.example.boomerang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoomerangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoomerangApplication.class, args);
+	}
+
+}
